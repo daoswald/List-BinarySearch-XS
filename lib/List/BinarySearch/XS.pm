5a75b42b4f3b2198ce663b3c482b7af2d96fbf00
David Oswald <daoswald@gmail.com>
2013-08-31 11:12:17 -0600

    Prepare CPAN dev release 0.01_004.

--- lib/List/BinarySearch/XS.pm
+++ lib/List/BinarySearch/XS.pm
@@ -15,7 +15,7 @@ our @EXPORT_OK = qw( binsearch binsearch_pos );
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 
-our $VERSION = '0.01_003';
+our $VERSION = '0.01_004';
 our $XS_VERSION = $VERSION;
 require XSLoader;
 XSLoader::load('List::BinarySearch::XS', $VERSION);
